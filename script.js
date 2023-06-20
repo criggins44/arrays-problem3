6a254365b2825c344426bacb7998beabbaaebb46
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-20 13:28:39 -0400

    added comments to explain logic

--- script.js
+++ script.js
@@ -2,21 +2,20 @@
 
 const myArray = [-3,8,7,6,5,-4,3,2,1];
 const secondTest = [11,13,5,2,7,19,21,-5];
-/*
-let experiment = myArray.sort((b, a) => {
-	if (a < b){return -1};
-	if (a > b){return 1};
-	return 0
-});
 
-console.log(experiment);
-*/
+
 myFunction = array => {
 	let experiment = array.sort((b, a) => {
+//adds comparator to help define how objects are ordered in the array
 	if (a < b){return -1};
 	if (a > b){return 1};
 	return 0
+/*conditions that outline how to order each 
+	object when they are compared to each other*/
+
   });
 	return experiment
+//returns the variable that holds the sort method
 }
+
 console.log(myFunction(myArray));
