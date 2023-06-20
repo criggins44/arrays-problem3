812055367b82c726f60e36b739b677a3aff09891
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-20 13:13:04 -0400

    added -1, 1 and 0 conditions to experiment with sorting

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 //Arrays Problem 3
-/*
-Define a function that takes in an array of numbers/integers and returns 
-the sorted version of that array, greatest to least. 
-(Note: do NOT use the "reverse" method.)
-
-Example array: const myArray = [-3,8,7,6,5,-4,3,2,1];
-
-Hint: To solve this, look up the JavaScript array sort method, 
-and pay close attention to how it uses its return 
-values of 1, 0, -1 to sort items
-*/
 
 const myArray = [-3,8,7,6,5,-4,3,2,1];
 const secondTest = [11,13,5,2,7,19,21,-5];
 
-//let newArray = secondTest.sort(function(a, b){return b-a});
+let experiment = myArray.sort((b, a) => {
+	if (a < b){return -1};
+	if (a > b){return 1};
+	return 0
+});
 
-//console.log(newArray);
+console.log(experiment);
 
 myFunction = array => {
-	
-	return (array.sort(function(a, b){b-a}));
+	let experiment = array.sort((b, a) => {
+	if (a < b){return -1};
+	if (a > b){return 1};
+	return 0
+  });
 }
-console.log(myFunction(secondTest));
