0c6a4ee3053d57a14ec49612741714ea9bc4a8fc
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-20 13:23:58 -0400

    added the function to accept arrays

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const myArray = [-3,8,7,6,5,-4,3,2,1];
 const secondTest = [11,13,5,2,7,19,21,-5];
-
+/*
 let experiment = myArray.sort((b, a) => {
 	if (a < b){return -1};
 	if (a > b){return 1};
@@ -10,11 +10,13 @@ let experiment = myArray.sort((b, a) => {
 });
 
 console.log(experiment);
-
+*/
 myFunction = array => {
 	let experiment = array.sort((b, a) => {
 	if (a < b){return -1};
 	if (a > b){return 1};
 	return 0
   });
+	return experiment
 }
+console.log(myFunction(myArray));
